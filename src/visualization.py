375f5a4a02e6a43d8133dabd25aefdbf39c5a1df
somu <ravurisomasekhar@gmail.com>
2026-04-06 11:47:12 +0530

    comment are been added

--- src/visualization.py
+++ src/visualization.py
@@ -13,3 +13,9 @@ def plot_monthly_sales(monthly_sales):
     plt.xlabel("Month")
     plt.ylabel("Sales")
     plt.show()
+
+
+
+    # these function aare used to shoe the
+    #  visualization of the data in the form f graph and charts.
+    #  it is used to show the top products and monthly sales trend.
